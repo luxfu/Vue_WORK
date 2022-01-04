84b3e4288a637c0a07019f7198f71ea30e0da77d
luxfu <407197339@qq.com>
2022-01-04 18:29:11 +0800

    20220104测试

--- demo/src/views/home.vue
+++ demo/src/views/home.vue
@@ -1,37 +1,19 @@
-<script setup>
-import { ref } from 'vue'
+<template>
+  <div>
+    <v-header />
+  </div>
+</template>
 
-defineProps({
-  msg: String
-})
+<script>
+import vHeader from "@/components/vHeader.vue";
 
-const count = ref(0)
+export default {
+  components: {
+    vHeader,
+  },
+};
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-</template>
 
 <style scoped>
 a {
